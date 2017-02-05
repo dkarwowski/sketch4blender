6929271b04db2956790a125d6fdbc1789d10b7e0
David Karwowski <karwowski.david@gmail.com>
2017-02-05 18:48:58 -0500

    lexer is working (more or less)
    * Split reserverd words out of the tokens.
    * Removed PAREN_ID and all keywords that expect parenthesis to act on it
      (makes the functions a little cleaner)
    * t_eof and t_error have been added
    * t_input needs to be set up more accurately

--- sketchl.py
+++ sketchl.py
@@ -1,45 +1,29 @@
 import ply.lex as lex
+from ply.lex import TOKEN
 
-reserved = {
-        'language'   : 'LANGUAGE'   ,
-        'pstricks'   : 'PSTRICKS'   ,
-        'tikz'       : 'TIKZ'       ,
-        'latex'      : 'LaTeX'      ,
-        'context'    : 'ConTeXt'    ,
-        'curve'      : 'CURVE'      ,
-        'camera'     : 'CAMERA'     ,
-        'def'        : 'DEF'        ,
-        'dots'       : 'DOTS'       ,
-        'frame'      : 'FRAME'      ,
-        'global'     : 'GLOBAL'     ,
-        'line'       : 'LINE'       ,
-        'polygon'    : 'POLYGON'    ,
-        'put'        : 'PUT'        ,
-        'repeat'     : 'REPEAT'     ,
-        'set'        : 'SET'        ,
-        'sweep'      : 'SWEEP'      ,
-        'then'       : 'THEN'       ,
-        'picturebox' : 'PICTUREBOX'
-}
+keyword_tokens = [ 'LANGUAGE'  , 'PSTRICKS' , 'TIKZ'    , 'LaTeX'      , 'ConTeXt'     ,
+                   'CURVE'     , 'CAMERA'   , 'DEF'     , 'DOTS'       , 'FRAME'       ,
+                   'GLOBAL'    , 'LINE'     , 'POLYGON' , 'PUT'        , 'REPEAT'      ,
+                   'SET'       , 'SWEEP'    , 'THEN'    , 'PICTUREBOX' , 'ASIN'        ,
+                   'ACOS'      , 'ATAN2'    , 'COS'     , 'INVERSE'    , 'PERSPECTIVE' ,
+                   'PROJECT'   , 'ROTATE'   , 'SCALE'   , 'SIN'        , 'SQRT'        ,
+                   'TRANSLATE' , 'UNIT'     , 'VIEW'    ]
+
+reserved = { keyword.lower() : keyword for keyword in keyword_tokens }
 
 # TODO(david): remove reserved words from having to be input manually
-tokens = ( 'ID'        , 'PAREN_ID'   , 'BRACKET_ID'  , 'DBL_BRACKET_ID' , 'CURLY_ID'    ,
-           'ANGLE_ID'  , 'NUM'        , 'OPTS_STR'    , 'SPECIAL'        , 'TICK'        ,
-           'THEN'      , 'DEF'        , 'EMPTY_ANGLE' , 'DOTS'           , 'LINE'        ,
-           'CURVE'     , 'POLYGON'    , 'REPEAT'      , 'SWEEP'          , 'PUT'         ,
-           'TRANSLATE' , 'ROTATE'     , 'SCALE'       , 'PROJECT'        , 'PERSPECTIVE' ,
-           'VIEW'      , 'SQRT'       , 'SIN'         , 'ASIN'           , 'COS'         ,
-           'ACOS'      , 'ATAN2'      , 'UNIT'        , 'INVERSE'        , 'GLOBAL'      ,
-           'SET'       , 'PICTUREBOX' , 'FRAME'       , 'CAMERA'         , 'LANGUAGE'    ,
-           'PSTRICKS'  , 'TIKZ'       , 'LaTeX'       , 'ConTeXt'        , 'NEG'         )
+tokens = [ 'ID'       , 'BRACKET_ID'  , 'DBL_BRACKET_ID' ,
+           'CURLY_ID' , 'ANGLE_ID' , 'NUM'         , 'OPTS_STR'       ,
+           'SPECIAL'  , 'TICK'     , 'EMPTY_ANGLE' ] + keyword_tokens
+
+literals = r"-+*/^|.()\[\]{}=,"
 
 state = (('input', 'exclusive'),)
 
-identifier = r'[A-Za-z]([A-Za-z0-9_]*[A-Za-z0-9])?'
-ws_not_nl  = r'[ \t\r]'
-ws         = r'[ \t\r\n]'
+Identifier = '[A-Za-z]([A-Za-z0-9_]*[A-Za-z0-9])?'
+WS         = '[ \t\r\n]'
 
-t_ignore = ws_not_nl
+t_ignore = ' \t\r'
 
 def t_comment(t):
     r'[%#].*'
@@ -55,12 +39,71 @@ def t_OPTS_STR(t):
     t.value = t.value[1:-1]
     return t
 
-@TOKEN(identifier)
+@TOKEN(r"'" + WS + r"*[xyz]")
+def t_TICK(t):
+    # set the index value
+    t.value = "xyz".index(t.value[1])
+    return t
+
+@TOKEN(r'(([0-9]+\.[0-9]*)|(\.[0-9]+)|([0-9]+))([eE][-+]?[0-9]+)?')
+def t_NUM(t):
+    t.value = float(t.value)
+    return t
+
+@TOKEN(r'input' + WS + r'*')
+def t_input(t):
+    # TODO(david): set up stack frame to parse into
+    raise NotImplementedError("need to recurse into file")
+    pass
+
+def _update_lineno(t):
+    t.lineno += sum([1 for c in t.value if c == '\n'])
+
+@TOKEN(r'special' + WS + r'*')
+def t_SPECIAL(t):
+    # TODO(david): add a warning
+    _update_lineno(t)
+    return t
+
+@TOKEN(Identifier)
 def t_ID(t):
     t.type = reserved.get(t.value, 'ID')
     return t
 
-@TOKEN(r'input' + ws + r'*')
-def t_input(t):
-    pass
+@TOKEN(r'<' + Identifier + r'>')
+def t_ANGLE_ID(t):
+    # strip the brackets off
+    t.value = t.value[1:-1]
+    return t
+
+@TOKEN(r'<>')
+def t_EMPTY_ANGLE(t):
+    return t
+
+@TOKEN(r'\[' + Identifier + r'\]')
+def t_BRACKET_ID(t):
+    t.value = t.value[1:-1]
+    return t
+
+@TOKEN(r'\[\[' + Identifier + r'\]\]')
+def t_DBL_BRACKET_ID(t):
+    t.value = t.value[2:-2]
+    return t
+
+@TOKEN(r'\{' + Identifier + r'\}')
+def t_CURLY_ID(t):
+    t.value = t.value[1:-1]
+    return t
+
+def t_error(t):
+    print("illegal character: '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+def t_eof(t):
+    return None
+
+lexer = lex.lex()
+
+if __name__=="__main__":
+    lex.runmain()
 
