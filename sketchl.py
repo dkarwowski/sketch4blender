3b20f20c0ca9397583e5fbea61baad680c537c0f
David Karwowski <karwowski.david@gmail.com>
2017-02-15 17:46:08 -0500

    inputs loaded
    the input directive loads files properly now, and will seamlessly
    continue the stream of tokens.

--- sketchl.py
+++ sketchl.py
@@ -1,3 +1,4 @@
+import os
 import ply.lex as lex
 from ply.lex import TOKEN
 
@@ -19,12 +20,17 @@ class Lexer:
 
     literals = r"-+*/^|.()\[\]{}=,"
 
-    state = (('input', 'exclusive'),)
+    states = (('input', 'inclusive'),)
 
     Identifier = '[A-Za-z]([A-Za-z0-9_]*[A-Za-z0-9])?'
     WS         = '[ \t\r\n]'
 
     t_ignore = ' \t\r'
+
+    def __init__(self):
+        self.lexer = []
+        self.filename = None
+        self.MAX_INPUT_DEPTH = 6
 
     def t_comment(self, t):
         r'[%#].*'
@@ -51,19 +57,18 @@ class Lexer:
         t.value = float(t.value)
         return t
 
+    def _update_lineno(self, t):
+        t.lineno += t.value.count('\n')
+
     @TOKEN(r'input' + WS + r'*')
     def t_input(self, t):
-        # TODO(david): set up stack frame to parse into
-        raise NotImplementedError("need to recurse into file")
-        pass
-
-    def _update_lineno(self, t):
-        t.lineno += sum([1 for c in t.value if c == '\n'])
+        self._update_lineno(t)
+        t.lexer.begin("input")
 
     @TOKEN(r'special' + WS + r'*')
     def t_SPECIAL(self, t):
         # TODO(david): add a warning
-        _update_lineno(self, t)
+        self._update_lineno(t)
         return t
 
     @TOKEN(Identifier)
@@ -96,17 +101,61 @@ class Lexer:
         t.value = t.value[1:-1]
         return t
 
+    # Input
+    def t_input_FILENAME(self, t):
+        r'''\{[^}]*\}'''
+        buf = ""
+        if self.filename:
+            buf = os.path.split(self.filename)[0] + os.sep
+        buf += t.value[1:-1] # cut off the braces
+
+        if len(self.lexer) < self.MAX_INPUT_DEPTH:
+            with open(buf, 'r') as f:
+                # create a new lexer with the same build
+                self.lexer.append(self.lexer[-1].clone())
+                self.lexer[-1].input(f.read())
+                self.lexer[-1].prev_filename = self.filename
+                # holds onto the state from last lexer (input)
+                self.lexer[-1].begin('INITIAL')
+                # stops returning tokens if nothing comes out of this
+                return self.lexer[-1].token()
+        # ensure lexer is in the right state when popping back
+        t.lexer.begin('INITIAL')
+
     def t_error(self, t):
         print("illegal character: '%s'" % t.value[0])
         t.lexer.skip(1)
 
     def t_eof(self, t):
-        return None
+        if len(self.lexer) == 1:
+            return None
+        # remember that last file name and pop back out
+        self.filename = self.lexer[-1].prev_filename
+        self.lexer.pop()
+        # return the next token to make sure we keep going
+        return self.lexer[-1].token()
 
     # build the lexer
     def build(self, **kwargs):
-        self.lexer = lex.lex(module=self, **kwargs)
-        return self.lexer
+        self.lexer.append(lex.lex(module=self, **kwargs))
+        return self
+
+    def input(self, s):
+        self.lexer[-1].input(s)
+
+    def token(self):
+        return self.lexer[-1].token()
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        t = self.token()
+        if t is None:
+            raise StopIteration
+        return t
+
+    __next__ = next
 
 if __name__=="__main__":
     lexer = Lexer().build()
